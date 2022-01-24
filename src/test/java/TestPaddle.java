bcf16c3d80e06040280e84505b7ad0a6214db220
Giulia Marchiori Pietrosanti <48836873+GiuliaMP@users.noreply.github.com>
2022-01-24 16:54:07 +0100

    From test to parametrized test.

--- src/test/java/TestPaddle.java
+++ src/test/java/TestPaddle.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestPaddle {
 
@@ -7,9 +9,9 @@ public class TestPaddle {
     private int y = 50;
     private Paddle p = new Paddle(1,x,y);
 
-    @Test
-    public void paddleMoveTest(){
-        int velocity = 1;
+    @ParameterizedTest
+    @ValueSource(ints={1,2,3,4,-4,20,-50})
+    public void paddleMoveTest(int velocity){
         p.setVelocity(velocity);
         p.move();
 
