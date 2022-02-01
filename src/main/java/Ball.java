489e753c69e5d5443c4dc5df0b583a9396d5b187
valebl <kiwi5.vb@gmail.com>
2022-02-01 12:31:28 +0100

    Added boundary checks in Ball and related tests.

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -2,19 +2,22 @@ public class Ball {
     
     private int x;
     private int y;
+    private int xVelocity;
+    private int yVelocity;
     private final int xStart;
     private final int yStart;
     private final int topLim;
     private final int botLim;
     private final int leftLim;
     private final int rightLim;
-    private int xVelocity;
-    private int yVelocity;
+
+    private final Paddle pLeft;
+    private final Paddle pRight;
 
     private final int BALL_RADIUS = 10;
 
 
-    public Ball(int topLim, int botLim, int leftLim, int rightLim) {
+    public Ball(int topLim, int botLim, int leftLim, int rightLim, Paddle pLeft, Paddle pRight) {
         xStart = (rightLim + leftLim) / 2 - BALL_RADIUS;
         yStart = (topLim + botLim) / 2 - BALL_RADIUS;
         x = xStart;
@@ -23,6 +26,8 @@ public class Ball {
         this.botLim = botLim;
         this.leftLim = leftLim;
         this.rightLim = rightLim;
+        this.pLeft = pLeft;
+        this.pRight = pRight;
         xVelocity = 0;
         yVelocity = 0;
     }
@@ -44,13 +49,33 @@ public class Ball {
 
     public void setyVelocity(int yVelocity) { this.yVelocity = yVelocity; }
 
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+
     public int getxStart() { return xStart; }
 
     public int getyStart() { return yStart; }
+
+    public int getBALL_RADIUS() { return BALL_RADIUS; }
 
     public void reset() {
         x = xStart;
         y = yStart;
     }
 
+    public boolean checkCollisionsTop() {
+        return y == (topLim + BALL_RADIUS);
+    }
+
+    public boolean checkCollisionsBottom() {
+        return y == (botLim - BALL_RADIUS);
+    }
+
+    public boolean checkCollisionsLeft() {
+        return x == (leftLim + BALL_RADIUS);
+    }
+
+    public boolean checkCollisionsRight() {
+        return x == (rightLim - BALL_RADIUS);
+    }
 }
--- src/test/java/TestBall.java
+++ src/test/java/TestBall.java
@@ -1,5 +1,7 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,8 +12,9 @@ public class TestBall {
     private int leftLim = 0;
     private int rightLim = 500;
 
-
-    private Ball ball = new Ball(topLim, botLim, leftLim, rightLim);
+    private final Paddle pLeft = new Paddle(0, topLim, botLim, leftLim, rightLim);
+    private final Paddle pRight = new Paddle(1, topLim, botLim, leftLim, rightLim);
+    private final Ball ball = new Ball(topLim, botLim, leftLim, rightLim, pLeft, pRight);
     private final int xStart = ball.getxStart();
     private final int yStart = ball.getyStart();
 
@@ -33,9 +36,54 @@ public class TestBall {
         ball.setxVelocity(xVelocity);
         ball.setyVelocity(yVelocity);
         ball.move();
-
         ball.reset();
+
         assertEquals(xStart, ball.getX());
         assertEquals(yStart, ball.getY());
     }
+
+    @ParameterizedTest
+    @ValueSource(ints={0, 10, 50, 250, 500})
+    public void ball_with_different_x_collides_with_top_boundary(int x) {
+        ball.setX(x);
+        ball.setY(topLim + ball.getBALL_RADIUS());
+
+        assertEquals(true, ball.checkCollisionsTop());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints={0, 10, 50, 250, 500})
+    public void ball_with_different_x_collides_with_bottom_boundary(int x) {
+        ball.setX(x);
+        ball.setY(botLim - ball.getBALL_RADIUS());
+
+        assertEquals(true, ball.checkCollisionsBottom());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints={0, 10, 50, 250, 500})
+    public void ball_with_different_y_collides_with_left_boundary(int y) {
+        ball.setX(leftLim + ball.getBALL_RADIUS()); // ball hits boundary with left side
+        ball.setY(y);
+
+        assertEquals(true, ball.checkCollisionsLeft());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints={0, 10, 50, 250, 500})
+    public void ball_with_different_y_collides_with_right_boundary(int y) {
+        ball.setX(rightLim - ball.getBALL_RADIUS()); // ball hits boundary with right side
+        ball.setY(y);
+
+        assertEquals(true, ball.checkCollisionsRight());
+    }
+
+/*    @ParameterizedTest
+    @CsvSource({"10, 50", "50, 100", "100, 30"})
+    public void ball_does_not_collide_with_top_boundary(int x, int y) {
+        ball.setX(x);
+        ball.setY(y);
+
+        assertEquals(false, ball.checkCollisionsTop());
+    }*/
 }
