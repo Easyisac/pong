f8c022a5eee82279b14fc72538bf61ae69ee3942
Giulia Marchiori Pietrosanti <48836873+GiuliaMP@users.noreply.github.com>
2022-02-01 15:58:31 +0100

    Bug fix.

--- src/main/java/BallDrawer.java
+++ src/main/java/BallDrawer.java
@@ -3,13 +3,16 @@ import java.awt.*;
 public class BallDrawer {
 
     private Ball ball;
+    private int ballRadius;
 
     public BallDrawer(Ball ball) {
+
         this.ball = ball;
+        ballRadius = ball.getBALL_RADIUS();
     }
 
     public void draw(Graphics2D g2){
         //g2.setColor(Color.red);
-        g2.fillRect(ball.getX(), ball.getY(), 20, 20); //ball height and width missing
+        g2.fillRect(ball.getX(), ball.getY(), ballRadius*2, ballRadius*2); //ball height and width missing
     }
 }
